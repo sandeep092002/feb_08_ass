99120c99f3ea32f1feac9c9d45efd87aa635cda0
sandeep092002 <99253463+sandeep092002@users.noreply.github.com>
2022-02-11 06:59:32 +0530

    Create passorfail.py

---
+++ passorfail.py
@@ -0,0 +1,11 @@
+s1=int(input("English marks="))
+s2=int(input("Telugu marks="))
+s3=int(input("Mathematics marks="))
+s4=int(input("Physics marks="))
+s5=int(input("Computer science marks="))
+s6=int(input("Analytical skills marks="))
+s7=int(input("Environmental studies marks="))
+s8=int(input("Online business="))
+if s1>35 and s2>35 and s3>35 and s4>35 and s5>35 and s6>17 and s7>17 and s8>17:
+    print("Pass")
+else:print("Fail")
