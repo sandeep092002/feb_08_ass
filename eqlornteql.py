f2abbbfdfb90df46eaf8a5431f92d08cf9c25fab
sandeep092002 <99253463+sandeep092002@users.noreply.github.com>
2022-02-12 22:49:26 +0530

    Create eqlornteql.py

---
+++ eqlornteql.py
@@ -0,0 +1,7 @@
+a=int(input('Enter a number a:'))
+b=int(input('Enter a number b:'))
+if a==b:
+    print('Both',a,b,'are equal')
+elif a>b:
+    print('Both are nor equal',a,'is greater than',b)
+else:print('Both are nor equal',a,'is less than',b)
